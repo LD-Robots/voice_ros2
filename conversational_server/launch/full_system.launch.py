208d6b1137a1a83ba47692f18e761932c83402fd
vale2506 <simavalentina.stefania@gmail.com>
2026-08-04 13:04:47 +0300

    [voice] remove node_suspend_manager_node from launch (executable does not exist)

--- conversational_server/launch/full_system.launch.py
+++ conversational_server/launch/full_system.launch.py
@@ -249,11 +249,5 @@ def generate_launch_description():
             parameters=[config_file_path]
         ),
 
-        Node(
-            package='conversational_client',
-            executable='node_suspend_manager_node',
-            name='node_suspend_manager_node',
-            parameters=[config_file_path]
-        ),
         ]),  # end GroupAction(namespace)
     ])
